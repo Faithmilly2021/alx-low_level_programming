532e971b6d454f0c98e76d7b4e1dcd3c55e891c5
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-08-29 20:27:09 +0200

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/ **
+*main - Entry point
+* prints "with proper grammar, but the output is a piece of art
+* Return : Always 0 (Success)
+* /
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art);return (0);
+}
