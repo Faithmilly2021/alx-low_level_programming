448ed98df4b33fc49d863fb24df6ea028052cca8
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-08-29 20:26:10 +0200

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/ **
+*main - Main function of my project
+*
+*
+*Return : Always 0 (Success)
+*/
+int main(void)
+{
+char displayed[52];
+  strcpy(displayed, " \ "Programming is like building a multilingual puzzle);
+  puts(displayed);
+return (0);
+}
