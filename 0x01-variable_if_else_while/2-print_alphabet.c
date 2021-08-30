58558586b92441ded3e7d105b740601220965efb
Faith <faithmncube48@gmail.com>
2021-08-30 13:35:58 -0700

    2 print alphabet

---
+++ 0x01-variable_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main-Main should contain putchar and a variable
+*Return:return 0 success
+*/
+
+int main(void)
+{
+int x;
+
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar(tolower(x));
+}
+putchar('\n');
+return (0);
+}
