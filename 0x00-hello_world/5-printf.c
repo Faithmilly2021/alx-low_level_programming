636ba9c9417f3d6c4e2805184168aa4a6c0ed5b6
Faith <faithmncube48@gmail.com>
2021-08-30 12:23:34 -0700

    5 -printf c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-/ **
-*main - Entry point
-* prints "with proper grammar, but the output is a piece of art
-* Return : Always 0 (Success)
-* /
+/**
+*main - prints the phrase with proper grammer
+*but the output is a piece of art
+*Return: always 0
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art);return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
