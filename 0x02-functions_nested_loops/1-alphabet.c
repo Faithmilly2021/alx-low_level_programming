cfa74d0da8dc7f6d485100a2cc0645fd091de73d
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-10-24 10:52:48 +0200

    1 alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
 
+#include "main.h"
+/**
+*print_alphabet - entry point
+*Description: prints the alphabet with _putchar
+*Return: void
+*/
+void print_alphabet(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
