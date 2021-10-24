100cd9896a77131a18a9b62e708e967dbcc8a23b
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-10-24 09:18:50 +0200

    Print base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main- Main have loop to print out base16
+*Return: Return 0 success
+*/
+int main(void)
+{
+int k;
+char ch;
+for (k = 48; k < 58; k++)
+{
+putchar(k);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
