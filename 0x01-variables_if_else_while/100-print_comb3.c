3e813bc2d181e90142ca19cb4e371e58ed3f7660
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-10-24 09:26:55 +0200

    print combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main- Main contain statement and loop
+*Return: return 0;
+*/
+int main(void)
+{
+int x, m;
+for (x = 48; x <= 56; x++)
+{
+for (m = 49; m <= 57; m++)
+{
+if (m > x)
+{
+putchar(x);
+putchar(m);
+if (x != 56 || m != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
