43496ff5c147b77340c6baaa015d36baaaf27ef3
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-10-24 10:51:57 +0200

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,18 @@
 
+#include "main.h"
+/**
+*main - Contain function and loop
+*Return: return 0 success
+*/
+int main(void)
+{
+char school[] = "_putchar";
+int i = 0;
+while (school[i] != '\0')
+{
+_putchar(school[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
