e9c8630bb9da63b5a49a7bef6e5cdd117e8208a5
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-10-24 09:08:11 +0200

    Update print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+#include <ctype.h>
+/**
+*main- Main have code that out use putchar
+*Return: return 0 success
+*/
+
+int main(void)
+{
+int x;
+for (x = 'A'; x <= 'Z'; x++)
+{
+putchar(tolower(x));
+}
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(toupper(x));
+}
+putchar('\n');
+return (0);
+}
