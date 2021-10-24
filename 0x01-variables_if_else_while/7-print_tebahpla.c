fcdc7b736f2dc760fe5f7082ea939f6f82adf9b0
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2021-10-24 09:15:42 +0200

    print tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,19 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+*main- Main have a for loop that reverse
+*Return: return 0 success
+*/
+
+int main(void)
+{
+int x;
+for (x = 'Z'; x >= 'A'; x--)
+{
+putchar(tolower(x));
+}
+putchar('\n');
+return (0);
+}
