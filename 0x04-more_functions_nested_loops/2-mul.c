bddc4006eb51f876079007955b05ae3293c6856e
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2022-04-03 14:05:58 +0200

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* mul - Entry point
+* Desc: A program to multiply two integers
+* @a: Data type checked
+* @b: Data type
+* Return: product of a and b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
