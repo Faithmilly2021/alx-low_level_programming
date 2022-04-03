0c7c1bb42a8d30bb53e56662160c07ce302b05a9
Faith Ncube <88321504+Faithmilly2021@users.noreply.github.com>
2022-04-03 14:09:13 +0200

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_numbers - Entry point
+* Return: void
+*/
+void print_numbers(void)
+{
+char b;
+for (b = '0'; b <= '9'; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
